2835e95e228c923821915a87a75f6df466192c47
nicol <t6104557@gmail.com>
2025-03-10 10:03:01 -0930

    operador and

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -7,5 +7,5 @@ Acorde a las tablas de verdad
 '''
 
 #Ejemplo 1:Operador not 
-y = not True 
-print("El resultado de operar con not es", y );
+y = True and True
+print("El resultado de operar con and es", y );
