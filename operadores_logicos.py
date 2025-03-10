eacb2d606fdde7a60c790fccac45bf009e495197
nicol <t6104557@gmail.com>
2025-03-10 12:47:01 -0930

    jerarquia

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -14,6 +14,24 @@ print("El resultado de operar con not es", y );
 y = False and True
 print("El resultado de operar con and es", y );
 
-#Ejemplo 1:Operador and
+#Ejemplo 3:Operador or
 y = False or True
 print("El resultado de operar con not es", y );
+
+'''
+Jerarquia de precedencia de operadores 
+(logicos inclusive)
+1.  ()
+2.  **
+3.  *, /, %
+4.  +,-
+5.  >, <, >=, <=, !=, ==
+6.  not
+7.  and
+8.  or
+9.  =
+'''
+
+#ejemplo 4: Jerarquia de operadores
+y = False and not True or False
+print("El resultado de operar con jerarquia de operadores es" ,y);
